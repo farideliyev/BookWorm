34e3662639fa666472ffdc62b5be115f227ed0e9
faridaliyev <ferid_577@mail.ru>
2021-06-09 20:14:19 +0200

    enabling localStorage

--- app.js
+++ app.js
@@ -12,24 +12,9 @@ class Book {
 
 class UI {
     static displayBooks() {
-        const data = [
-            {
-                title: 'BOOK1',
-                author: 'Farid Aliyev',
-                isbn: '1234567'
-            },
+        const data = Store.getBooks()
 
-            {
-                title: 'BOOK2',
-                author: 'Farid Aliyev',
-                isbn: '7654321'
-            },
-
-        ]
-
-        const books = data
-
-        books.forEach(book=>UI.addBookToList(book))
+        data.forEach(book=>UI.addBookToList(book))
     }
 
     static addBookToList (book) {
@@ -38,10 +23,37 @@ class UI {
         row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
-        <td>${book.isbn}</td>
+        <td id="isbn">${book.isbn}</td>
         <td><button class="btn btn-warning btn-sm delete">X</button></td>
         `
         list.appendChild(row)
+
+
+    }
+
+    static showAlert(message, alertType){
+        const div = document.createElement("div")
+        div.className = `alert alert-${alertType}`
+        div.appendChild(document.createTextNode(message))
+        const container = document.querySelector(".container")
+        const form = document.querySelector("#book-form")
+        container.insertBefore(div, form)
+
+
+        setTimeout(()=>{
+
+            document.querySelector(".alert").remove()
+        }, 3000)
+
+        setTimeout(()=>{
+            document.querySelector(".alert").style.opacity = "0"
+        }, 1000)
+
+    }
+
+    static deleteBook (el) {
+        el.classList.contains("delete") && el.parentElement.parentElement.remove()
+
     }
 
     static clearFields() {
@@ -52,6 +64,41 @@ class UI {
 }
 
 // STORE CLASS
+
+class Store {
+
+    static getBooks(){
+        let books;
+        if(localStorage.getItem("books") === null) {
+            books = []
+        } else {
+            console.log(JSON.parse(localStorage.getItem('books')))
+            books = JSON.parse(localStorage.getItem('books'))
+        }
+
+        return books
+
+    }
+
+    static addBook(book){
+        const books = Store.getBooks()
+        books.push(book)
+        localStorage.setItem('books', JSON.stringify(books))
+    }
+
+
+    static deleteBook(isbn){
+        console.log(isbn)
+        const books = Store.getBooks()
+        books.forEach((book, index)=>{
+            if(book.isbn === isbn){
+                books.splice(index, 1)
+            }
+        })
+
+        localStorage.setItem('books', JSON.stringify(books))
+    }
+}
 
 //Event: Display books
 document.addEventListener("DOMContentLoaded", UI.displayBooks)
@@ -65,14 +112,40 @@ document.querySelector("#book-form").addEventListener("submit", (e)=>{
     let author = document.querySelector("#bookAuthor").value
     let isbn = document.querySelector("#bookISBN").value
 
-    // Instantiate New Book
-    let book = new Book(title, author, isbn)
-    console.log(book)
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert("Please fill in all fields", 'danger')
 
-    // Add Book to List
-    UI.addBookToList(book)
+    } else {
+        // Instantiate New Book
+        let book = new Book(title, author, isbn)
 
-    // Clear fields data
-    UI.clearFields()
+        // Add Book to List
+        UI.addBookToList(book)
+
+        // Add Book To Local Storage
+
+        Store.addBook(book)
+
+        //Show success message
+        UI.showAlert("Book was successfully added to list", "success")
+
+        // Clear fields data
+        UI.clearFields()
+    }
+
+
 })
 //Event: Remove a book
+
+document.querySelector("#book-list").addEventListener("click", (e)=>{
+    //delete message
+    UI.deleteBook(e.target)
+
+    //delete from Local Storage
+    let isbn = e.target.parentElement.previousElementSibling.textContent
+   Store.deleteBook(isbn)
+
+    //  Show delete alert
+
+    UI.showAlert("Book was deleted" , "info")
+})
